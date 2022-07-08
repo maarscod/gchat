d3238cdc5d16a1c6a2d1046b3b5c57db019ec017
maars <osg1532@gmail.com>
2022-07-08 17:28:44 +0200

    🚚 update import

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ if (isLocalEnv) {
 const express = require("express");
 const mongoose = require("mongoose");
 
-const indexRouter = require("./route/index");
+const indexRouter = require("./route/message");
 
 const app = express();
 
